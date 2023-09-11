7ecbadb62e93c2db84669eebd7ab405e242d0293
Anand Kothapeta <kothapet@hotmail.com>
2023-09-11 17:54:59 -0400

    Working KPL and localstack

--- src/main/java/com/github/kothapet/scs/config/LocalStackClientConfiguration.java
+++ src/main/java/com/github/kothapet/scs/config/LocalStackClientConfiguration.java
@@ -1,11 +1,24 @@
 package com.github.kothapet.scs.config;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.cloudwatch.AmazonCloudWatchAsync;
+import com.amazonaws.services.cloudwatch.AmazonCloudWatchAsyncClientBuilder;
+import com.amazonaws.services.kinesis.AmazonKinesisAsync;
+import com.amazonaws.services.kinesis.AmazonKinesisAsyncClientBuilder;
+import com.amazonaws.services.kinesis.producer.KinesisProducer;
 import com.amazonaws.services.kinesis.producer.KinesisProducerConfiguration;
+import com.amazonaws.services.securitytoken.AWSSecurityTokenServiceAsync;
+import com.amazonaws.services.securitytoken.AWSSecurityTokenServiceAsyncClientBuilder;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,30 +26,65 @@ import lombok.RequiredArgsConstructor;
 @Configuration
 @RequiredArgsConstructor
 public class LocalStackClientConfiguration {
-    /*
-    @Bean
-    public KinesisProducer kinesisProducer() {
-        KinesisProducerConfiguration configuration = new KinesisProducerConfiguration()
-            .setKinesisEndpoint("localhost")
-            .setKinesisPort(4567)
-            .setCloudwatchEndpoint("localhost")
-            .setCloudwatchPort(4567)
-            .setVerifyCertificate(false)
-            .setRegion("us-east-1");
 
-        return new KinesisProducer(configuration);
-    }
-    */
+	@Value("${cloud.aws.region.static}")
+	private String region;
+
+	@Value("${spring.cloud.aws.sts.endpoint}")
+	private String stsEndpoint;
+
+	@Value("${spring.cloud.aws.kinesis.endpoint}")
+	private String kinesisEndpoint;
+
+	@Value("${spring.cloud.aws.cloudwatch.endpoint}")
+	private String cloudWatchEndpoint;
+
+	@Value("${spring.cloud.aws.dynamodb.endpoint}")
+	private String dynamoDbEndpoint;
+
+	private AWSCredentialsProvider getCredentialsProvider() {
+
+		AWSCredentialsProvider credentialsProvider = new DefaultAWSCredentialsProviderChain();
+
+		System.out.println("getAWSAccessKeyId : " + credentialsProvider.getCredentials().getAWSAccessKeyId());
+		System.out.println("getAWSSecretKey   : " + credentialsProvider.getCredentials().getAWSSecretKey());
+		return credentialsProvider;
+	}
+/*
 	@Bean
-	public KinesisProducerConfiguration kinesisProducerConfiguration()  {
-			return new KinesisProducerConfiguration()
-					.setCredentialsProvider(new DefaultAWSCredentialsProviderChain())
-					.setRegion("us-east-1")
-					.setKinesisEndpoint("localhost")
-					.setKinesisPort(4567)
-					.setCloudwatchEndpoint("localhost")
-					.setCloudwatchPort(4567)
-					.setVerifyCertificate(false);
-	}	
-	
+	public AWSSecurityTokenServiceAsync aWSSecurityTokenServiceAsync() {
+		System.out.println("**** AWSSecurityTokenServiceAsync ***");
+		return AWSSecurityTokenServiceAsyncClientBuilder.standard()
+				.withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(stsEndpoint, region)).build();
+	}
+
+	@Bean
+	public AmazonKinesisAsync amazonKinesisAsync() {
+		System.out.println("**** amazonKinesisAsync ***");
+		return AmazonKinesisAsyncClientBuilder.standard().withCredentials(getCredentialsProvider())
+				.withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(kinesisEndpoint, region)).build();
+	}
+
+	@Bean
+	public AmazonCloudWatchAsync amazonCloudWatchAsync() {
+		System.out.println("**** amazonCloudWatchAsync ***");
+		return AmazonCloudWatchAsyncClientBuilder.standard().withCredentials(getCredentialsProvider())
+				.withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(cloudWatchEndpoint, region))
+				.build();
+	}
+*/
+
+	@Bean
+	public KinesisProducerConfiguration kinesisProducerConfiguration() throws URISyntaxException {
+		System.out.println("**** KinesisProducerConfiguration ***");
+		URI stsURI = new URI(stsEndpoint);
+		URI kinesisURI = new URI(kinesisEndpoint);
+		URI cloudWatchURI = new URI(cloudWatchEndpoint);
+		return new KinesisProducerConfiguration().setCredentialsProvider(getCredentialsProvider())
+				.setStsEndpoint(stsURI.getHost()).setStsPort(stsURI.getPort())
+				.setKinesisEndpoint(kinesisURI.getHost()).setKinesisPort(kinesisURI.getPort())
+				.setCloudwatchEndpoint(cloudWatchURI.getHost()).setCloudwatchPort(cloudWatchURI.getPort())
+				.setRegion(region).setVerifyCertificate(false);
+	}
+
 }
